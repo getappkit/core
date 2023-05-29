448801b4a375dc72bc22a669b6e417ed9e316dfe
Maarten Thiebou <getappkit@gmail.com>
2023-05-29 02:21:32 +0200

    Update Psr17FactoryTest.php

--- tests/Http/Factory/Psr17FactoryTest.php
+++ tests/Http/Factory/Psr17FactoryTest.php
@@ -5,32 +5,50 @@ namespace Http\Factory;
 
 use Appkit\Http\Factory\Psr17Factory;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\RequestInterface;
 
 class Psr17FactoryTest extends TestCase
 {
+    protected $factory;
+
+    protected function setUp(): void
+    {
+        $this->factory = new Psr17Factory();
+    }
+
+    public function testCreateRequest()
+    {
+        $request = $this->factory->createRequest('GET', 'http://example.com');
+
+        $this->assertInstanceOf(RequestInterface::class, $request);
+        $this->assertEquals('GET', $request->getMethod());
+        $this->assertEquals('http://example.com', (string) $request->getUri());
+    }
+
+
     public function testCreateResponse()
     {
-        $factory = new Psr17Factory();
-        $r = $factory->createResponse(200);
+
+        $r = $this->factory->createResponse(200);
         $this->assertEquals('OK', $r->getReasonPhrase());
 
-        $r = $factory->createResponse(200, '');
+        $r = $this->factory->createResponse(200, '');
         $this->assertEquals('', $r->getReasonPhrase());
 
-        $r = $factory->createResponse(200, 'Foo');
+        $r = $this->factory->createResponse(200, 'Foo');
         $this->assertEquals('Foo', $r->getReasonPhrase());
 
         /*
          * Test for non-standard response codes
          */
-        $r = $factory->createResponse(567);
+        $r = $this->factory->createResponse(567);
         $this->assertEquals('', $r->getReasonPhrase());
 
-        $r = $factory->createResponse(567, '');
+        $r = $this->factory->createResponse(567, '');
         $this->assertEquals(567, $r->getStatusCode());
         $this->assertEquals('', $r->getReasonPhrase());
 
-        $r = $factory->createResponse(567, 'Foo');
+        $r = $this->factory->createResponse(567, 'Foo');
         $this->assertEquals(567, $r->getStatusCode());
         $this->assertEquals('Foo', $r->getReasonPhrase());
     }
