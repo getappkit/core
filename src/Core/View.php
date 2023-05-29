ac6d5befc1de4a456fef30dac3f9dad8a6d3f158
Maarten Thiebou <getappkit@gmail.com>
2023-05-29 21:42:16 +0200

    Update View.php

--- src/Core/View.php
+++ src/Core/View.php
@@ -19,19 +19,6 @@ use Throwable;
 class View
 {
     public static array $data = [];
-    /**
-     * Render a view file
-     *
-     * @param string $view The view file
-     * @param array $args Associative array of data to display in the view (optional)
-     *
-     * @return string
-     * @throws Throwable
-     */
-    public static function render(string $view, array $args = []): string
-    {
-        return Tpl::load(Roots::VIEWS . DS . $view . '.php', $args);
-    }
 
     /**
      * Render a view snippet
@@ -45,7 +32,7 @@ class View
     {
         $data = array_merge($data, static::$data);
 
-        return Tpl::load(Roots::SNIPPETS . DS . $name . '.php', $data);
+        return Tpl::load(Roots::SNIPPETS . '/' . $name . '.php', $data);
     }
 }
 View::$data['site'] = (object)Config::get('site');
